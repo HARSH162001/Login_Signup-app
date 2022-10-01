b86d74f488134bae54b9633e77a1df7277417286
HARSH162001 <harshbtech00@gmail.com>
2022-10-01 07:46:06 +0530

    day2 - fixed UI

---
+++ src/common/button.js
@@ -0,0 +1,12 @@
+module.exports ={
+button1:{
+    backgroundColor: '#FF1616',
+    color: '#fff',
+    padding: 10,
+    borderRadius: 5,
+    fontsize: 20,
+    minWidth: 150,
+    textAlign: 'center',
+    margin: 10,
+  } 
+}
---
+++ src/common/formcss.js
@@ -0,0 +1,49 @@
+module.exports ={
+    head1:{
+        fontSize: 30,
+        color:'black',
+        textAlign:'center',
+        fontWeight:'500',
+    },
+    head2:{
+        fontSize: 20,
+        color:'black',
+        textAlign:'center',
+    },
+    formgroup:{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        marginVertical: 6,
+      },
+      lable:{
+        fontSize: 17,
+        color: '#F50057',
+        marginLeft: 10,
+        marginBottom: 3,
+      },
+      input:{
+        backgroundColor: '#E490C4',
+        borderRadius: 20,
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+      },
+      link:{
+         color: '#E490C4',
+         fontSize: 17,
+      },
+        link2:{
+          color:'grey',
+          fontSize:15,
+          textAlign:'center',
+          marginVertical:10,
+     },
+     input1:{
+        backgroundColor: '#E490C4',
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 7,
+        height: 100,
+        textAlignVertical: 'top',
+      },
+    }
